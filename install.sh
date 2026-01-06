55afd1bf568b35d787ce1cdc297a43390a27b53f
KP <lozadaerwin242@gmail.com>
2026-01-06 00:38:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Color Verde
-VERDE='\033[0;32m'
+VERDE='\033[1;32m'
 NC='\033[0m'
 
-echo -e "${VERDE}"
-echo "------------------------------------------------"
-echo "       INSTALANDO TECNO TOOL KP..."
-echo "------------------------------------------------"
+echo -e "${VERDE}[+] Iniciando instalación de TecnoTool v1.0...${NC}"
 
-pkg update -y
+# Actualizar e instalar dependencias
+pkg update -y && pkg upgrade -y
 pkg install android-tools curl -y
 
-# Instalando el ejecutable
+# Configurar el ejecutable en el sistema
+echo -e "${VERDE}[+] Configurando comando global...${NC}"
 curl -L "https://raw.githubusercontent.com/Optimizadorww/TECNO-TOOL-KP/main/Tool.sh" -o $PREFIX/bin/TecnoTool
 chmod +x $PREFIX/bin/TecnoTool
 
-echo ""
-echo " CONFIGURACIÓN COMPLETADA."
-echo " Ahora puedes escribir: TecnoTool"
-echo "------------------------------------------------"
-echo -e "${NC}"
+echo -e "\n${VERDE}=========================================="
+echo "    INSTALACIÓN COMPLETADA CON ÉXITO"
+echo "    Escribe 'TecnoTool' para iniciar"
+echo -e "==========================================${NC}"
